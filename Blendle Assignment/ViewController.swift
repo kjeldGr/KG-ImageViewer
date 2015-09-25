4ca666424995d1c5dcf342c50b56afa8aa8092a5
Kjeld Groot <kjeldgroot@gmail.com>
2015-09-25 14:19:44 +0200

    Initial Commit

---
+++ Blendle Assignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Blendle Assignment
+//
+//  Created by Kjeld Groot on 25-09-15.
+//  Copyright © 2015 KjeldGr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
