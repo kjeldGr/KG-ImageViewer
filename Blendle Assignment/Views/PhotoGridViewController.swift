f11832580a820d1be474e114486580d1c12850f5
Kjeld Groot <kjeldgroot@gmail.com>
2015-09-25 15:32:49 +0200

    Pod configuration and file structure

---
+++ Blendle Assignment/Views/PhotoGridViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PhotoGridViewController.swift
+//  Blendle Assignment
+//
+//  Created by Kjeld Groot on 25-09-15.
+//  Copyright © 2015 KjeldGr. All rights reserved.
+//
+
+import UIKit
+
+class PhotoGridViewController: BlendleViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'Blendle Assignment' do
+
+pod 'Spring'
+pod 'Alamofire', '~> 2.0'
+pod 'SVProgressHUD'
+pod 'RazzleDazzle'
+
+end
+
